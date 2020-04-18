d3817153b3c7be4147ef8b71b88d41bfeacdf75e
kuvanpt2012 <kboylovemam@gmail.com>
2020-04-18 20:29:18 +0700

    .

--- app/src/main/java/com/example/progressbarseekbar/MainActivity.java
+++ app/src/main/java/com/example/progressbarseekbar/MainActivity.java
@@ -5,15 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 //    ProgressBar mProgressbar;
+    SeekBar mSeekbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSeekbar = findViewById(R.id.seekbar);
+
+        mSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Toast.makeText(MainActivity.this,i + "", Toast.LENGTH_SHORT);
+
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 //        mProgressbar = findViewById(R.id.progressbar);
 //
