bdaa4ccfb378d0534c52480217517787a404c46f
kuvanpt2012 <kboylovemam@gmail.com>
2020-04-18 20:17:57 +0700

    process bar

--- app/src/main/java/com/example/progressbarseekbar/MainActivity.java
+++ app/src/main/java/com/example/progressbarseekbar/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.progressbarseekbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar mProgressbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mProgressbar = findViewById(R.id.progressbar);
+
+
+
+        CountDownTimer countDownTimer = new CountDownTimer(1000 , 1000) {
+            @Override
+            public void onTick(long l) {
+                if (mProgressbar.getProgress() >= 100){
+                    mProgressbar.setProgress(0);
+                }
+                mProgressbar.setProgress(mProgressbar.getProgress() + 10);
+
+
+            }
+
+            @Override
+            public void onFinish() {
+                this.start();
+
+            }
+        };
+        countDownTimer.start();
+
+
     }
+
 }
